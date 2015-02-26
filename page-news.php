53bb9018bf68ae6b9ef1b6501b2c69f6fffd5726
Admin <powerlogic1992@gmail.com>
2015-02-26 21:52:52 +0800

    Ox Oil Theme

--- page-news.php
+++ page-news.php
@@ -12,7 +12,7 @@ get_header();
         <div class="row">
 
             <div class="col-md-8">
-                <div class="well well-products well-main" style="min-height: 690px;">
+                <div class="well well-products well-main" style="min-height: 640px;">
                     <div class="row">
                         <div class="col-md-2" style="padding-right: 0;  margin-top: 25px;"><h2 class="title">News <i class="fa fa-angle-double-right"></i> </h2></div>
                         <div class="col-sm-9 news-list" style="padding-left: 0;">
