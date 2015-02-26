81f89036dc4d21d5f6aa35f5cd29da7e157663ee
Admin <powerlogic1992@gmail.com>
2015-02-26 16:41:28 +0800

    Ox Oil Theme

--- page-news.php
+++ page-news.php
@@ -14,8 +14,19 @@ get_header();
             <div class="col-md-8">
                 <div class="well well-products well-main" style="min-height: 285px;">
                     <div class="row">
-                        <div class="col-md-12"><h2 class="title">Oil Products <i class="fa fa-angle-double-right"></i> </h2></div>
-                        asdasdas
+                        <div class="col-md-12"><h2 class="title">News <i class="fa fa-angle-double-right"></i> </h2></div>
+                        <div class="col-sm-12 news-list">
+                            <h2 class="title news-title">Oil & Gas</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <?php for($i = 0; $i < 5; $i++):?>
+                                <div class="row news-item">
+                                    <div class="col-md-12">
+                                        <div class="news-date"><i class="fa fa-clock-o"></i> 2 hours ago</div>
+                                        <div class="news-item-title">Transocean posts loss on important charge</div>
+                                    </div>
+                                </div>
+                            <?php endfor ?>
+                        </div>
                     </div>
                 </div>
             </div>
