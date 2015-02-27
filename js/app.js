a144ca3acb853e14cf698ab2fc2bc1291a81780e
Admin <powerlogic1992@gmail.com>
2015-02-27 10:50:08 +0800

    Ox Oil Theme

--- js/app.js
+++ js/app.js
@@ -7,8 +7,9 @@ app.controller('MainController', function( $scope ){
 app.controller('FeatureProductsController', function( $scope ){
 
     $scope.featureProducts = [
-        { id: 1, title : 'Base Oil', photo:  'http://www.severnfuels.co.uk/images/gallery/road_forecourt_key_fuels.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
-        { id: 2, title : 'Fuel with Sulfur', photo:  'http://www.severnfuels.co.uk/images/gallery/road_forecourt_key_fuels.jpg', description: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' }
+        { id: 1, title : 'Base Oil', photo:  'http://www.argusmedia.com.br/~//~/media/Images/Hero-Box-530-by-335/Petroleum/Base-Oil.jpg', description: 'Base oils are used to manufacture products including lubricating greases, motor oil and metal processing fluids. Different products require different compositions and properties in the oil. One of the most important factors is the liquid’s viscosity at various temperatures.' },
+        { id: 2, title : 'Fuel with Sulfur', photo:  'http://www.severnfuels.co.uk/images/gallery/road_forecourt_key_fuels.jpg', description: 'Fuels are any materials that store potential energy in forms that can be practicably released and used for work or as heat energy. ' },
+        { id: 3, title : 'Gasoline', photo:  'http://www.thehindu.com/multimedia/dynamic/00497/IN08_GASOLINE1_497253f.jpg', description: 'Gasoline, also spelled gasolene, also called gas or petrol, mixture of volatile, flammable liquid hydrocarbons derived from petroleum and used as fuel for internal-combustion engines. It is also used as a solvent for oils and fats.' }
     ];
     $scope.currentProduct = $scope.featureProducts[0];
     $scope.isCurrentlyActive = $scope.currentProduct.id ==  $scope.featureProducts[0].id ? true : false;
