979333728aaa2cc375b5a36ad73f7cef564590ec
Admin <powerlogic1992@gmail.com>
2015-02-27 16:09:19 +0800

    Added Page Test.

--- page-test.php
+++ page-test.php
@@ -35,7 +35,7 @@ function getQuote($symbol)
 
 //trace( getQuote("CLG12.ENYM") ); //this is the stock symbol
 //trace( getQuote("CLJ15%2NYM") ); //this is the stock symbol
-trace("http://download.finance.yahoo.com/d/quotes.csv?s=%40%5EDJI,GOOG&f=nsl1op");
+trace(fopen( "http://download.finance.yahoo.com/d/quotes.csv?s=%40%5EDJI,GOOG&f=nsl1op" ,"r"));
 ?>
 
 <?php get_footer(); ?>
