550c6338acee0d87c2fb228be17ac35656566141
Admin <powerlogic1992@gmail.com>
2015-02-28 21:28:09 +0800

    Added Page Test.

--- page-test.php
+++ page-test.php
@@ -16,13 +16,23 @@ foreach($metalSymbols as $symbol){
 //trace($metalQuotes);
 
 //crude oil, gasoline, fuel, lpg
+$energyNames = ['Crude Oil', 'Gasoline', 'Fuel', 'LPG'];
 $energySymbols = ['UCO', 'UGA', 'FUEL', 'LPG'];
 $energyQuotes = array();
-foreach( $energySymbols  as $symbol ){
-    $energyQuotes[] = yfGetQuote($symbol);
+foreach( $energySymbols  as $i => $symbol ){
+    $quote = yfGetQuote($quote);
+
+    $name = $energyNames[$i];
+    $percent = yfGetPercent($quote['previousClose'], $quote['last']);
+    $status = $percent < 0 ? 'down' : 'up';
+    $last = $quote['last'];
+
+    $energyQuotes[] = $quote;
+
+
+    echo '<span class="item">'.$name.' <span class="stat"><i class="fa fa-arrow-'.$status.'"></i>'.$last.' ('.str_replace('-','',$percent).'%)</span> </span>';
 }
 
-$quoteData = yfGetQuote('LPG');
 trace( yfGetPercent($quoteData['previousClose'], $quoteData['last']) ); //this is the stock symbol ?>
 
 <?php get_footer(); ?>
