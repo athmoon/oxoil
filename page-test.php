d7fb8c06631978393159be4d92e536e25b00a7b5
Admin <powerlogic1992@gmail.com>
2015-02-28 14:48:49 +0800

    Added Page Test.

--- page-test.php
+++ page-test.php
@@ -12,8 +12,8 @@ function getQuote($symbol)
 {
 
     //$symbol  = urlencode( trim( substr(strip_tags($symbol),0,7) ) );
-    $yahooCSV = "http://download.finance.yahoo.com/d/quotes.csv?s=GLD,SCO&f=sl1d1t1c1ohgvpnbaejkr";
-    //$yahooCSV = "http://finance.yahoo.com/d/quotes.csv?s=%40%$symbol&f=sl1d1t1c1ohgvpnbaejkr&o=t";
+    //$yahooCSV = "http://download.finance.yahoo.com/d/quotes.csv?s=GLD,SCO&f=sl1d1t1c1ohgvpnbaejkr";
+    $yahooCSV = "http://finance.yahoo.com/d/quotes.csv?s=$symbol&f=sl1d1t1c1ohgvpnbaejkr&o=t";
 
     $csv = fopen($yahooCSV,"r");
 
@@ -33,7 +33,8 @@ function getQuote($symbol)
     }
 }
 
-trace( getQuote("CLJ15%2NYM") ); //this is the stock symbol
+trace( getQuote("GLD") ); //this is the stock symbol
+trace( getQuote("SCO") ); //this is the stock symbol
 ?>
 
 <?php get_footer(); ?>
