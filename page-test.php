3eeba2c74f0bedf103c398d2f5bce5aee6679777
Admin <powerlogic1992@gmail.com>
2015-02-28 18:56:24 +0800

    Added Page Test.

--- page-test.php
+++ page-test.php
@@ -6,20 +6,25 @@ Author: Juni Brosas @ iBoostme..
 get_header();
 ?>
 <?php
-//gold, copper, silver, platinum, palladium, zinc
-$metalSymbols = ['GLD', 'SCCO', 'SLV', 'PLG', 'PAL', 'CZN.TO'];
+//gold, copper, silver, platinum, palladium, zinc, aluminum
+$metalSymbols = ['GLD', 'SCCO', 'SLV', 'PLG', 'PAL', 'CZN.TO', 'CENX'];
 $metalQuotes = array();
 foreach($metalSymbols as $symbol){
     $metalQuotes[] = yfGetQuote($symbol);
 }
 
-trace($metalQuotes);
+//trace($metalQuotes);
 
-//crude oil
-$energySymbols = ['UCO'];
+//crude oil, gasoline, fuel, lpg
+$energySymbols = ['UCO', 'UGA', 'UGA', 'FUEL', 'LPG'];
+$energyQuotes = array();
+foreach( $energySymbols  as $symbol ){
+    $energyQuotes[] = yfGetQuote($symbol);
+}
 
-$quoteData = yfGetQuote("SCO");
-trace( yfGetPercent($quoteData['previousClose'], $quoteData['last']) ); //this is the stock symbol
+trace($energyQuotes);
+
+//trace( yfGetPercent($quoteData['previousClose'], $quoteData['last']) ); //this is the stock symbol
 
 ?>
 
