9a7600e40d1bc5c80bfe9a701f846819579ee161
Admin <powerlogic1992@gmail.com>
2015-02-28 21:46:16 +0800

    Added Page Test.

--- widgets/widget-ticker.php
+++ widgets/widget-ticker.php
@@ -3,7 +3,7 @@
         <?php
         //crude oil, gasoline, fuel, lpg
         $energyQuotes = getEnergyQuotes();
-        foreach( $energyQuotes  as $i => $symbol ){
+        foreach( $energyQuotes  as $i => $quote ){
             echo '<span class="item">'.$quote['descriptiveName'].' <span class="stat"><i class="fa fa-arrow-'.$quote['arrowStatus'].'"></i>'.$quote['last'].' ('.str_replace('-','',$quote['currentPercent']).'%)</span> </span>';
         }
         ?>
