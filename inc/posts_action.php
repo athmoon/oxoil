31f481cd376c4e1af0dd5c31c822c0d68a0cc782
Admin <powerlogic1992@gmail.com>
2015-03-01 22:59:01 +0800

    Added Page Test.

--- inc/posts_action.php
+++ inc/posts_action.php
@@ -3,7 +3,7 @@
 	function get_post_content( $params )
 	{
 		$post_id  = $params['id'];
-		return $post_id;
+
 
 		/*$post     = get_post( $post_id );
 		$template = get_field( 'template', $post_id );*/
