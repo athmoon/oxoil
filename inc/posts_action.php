a48325fa1b471860ac72a94b7ea428196ef51e9c
Admin <powerlogic1992@gmail.com>
2015-03-01 22:21:54 +0800

    Added Page Test.

--- inc/posts_action.php
+++ inc/posts_action.php
@@ -3,8 +3,10 @@
 	function get_post_content( $params )
 	{
 		$post_id  = $params['id'];
-		$post     = get_post( $post_id );
-		$template = get_field( 'template', $post_id );
+		return $post_id;
+
+		/*$post     = get_post( $post_id );
+		$template = get_field( 'template', $post_id );*/
 
 		echo 'asdasd=';
 		die();
