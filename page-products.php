ef97743369758b0861aeaf673a75b20eb936784a
Admin <powerlogic1992@gmail.com>
2015-03-01 18:44:59 +0800

    Added Page Test.

--- page-products.php
+++ page-products.php
@@ -17,7 +17,8 @@
 						<div class="row">
 							<div class="col-md-12"><h2 class="title">Oil Products <i class="fa fa-angle-double-right"></i> </h2></div>
 							<div class="col-sm-5" >
-								<?=get_the_post_thumbnail($products[0]->ID)?>
+
+								<?=get_the_post_thumbnail($products[0]->ID, '', ['class' => 'img-responsive'])?>
 								<!--<a href="#">
 									<img class="img-responsive preview-product" src="http://www.severnfuels.co.uk/images/gallery/road_forecourt_key_fuels.jpg">
 								</a>-->
