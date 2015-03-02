740deb87384aa12313ce9c686217744f97260aad
Admin <powerlogic1992@gmail.com>
2015-03-02 16:52:28 +0800

    Added Page Test.

--- 404.php
+++ 404.php
@@ -12,11 +12,11 @@
 							<span><b>Page Not Found </b>Error 404</h2>
 						<div class="space-md"></div>
 						<div class="row">
-							<div class="" >
+							<div style="min-width: 300px;" >
 								<p>The page you requested could not be found, either contact your webmaster or try again. Use your browsers <b>Back</b> button to navigate to the page you have prevously come from</p>
 								<div class="space-lg"></div>
-								<div class="text-center">
-									<a href="<?=get_site_url()?>" class="btn btn-primary btn-blue"><i class="icon-home icon-white"></i> Take Me Home</a>
+								<div class="text-center margin" style="margin-top: 40px;">
+									<a href="<?=get_site_url()?>" class="btn btn-primary btn-blue link"><i class="icon-home icon-white"></i> Take Me Home</a>
 								</div>
 							</div>
 						</div>
