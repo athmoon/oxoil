a48c55567f7963971261f7aacd0fc9e2befaa58e
Admin <powerlogic1992@gmail.com>
2015-03-02 17:42:25 +0800

    Added Page Test.

--- page-underconstruction.php
+++ page-underconstruction.php
@@ -13,8 +13,9 @@ get_header();
                 <div class="col-md-12">
                     <div class="well well-products well-main" style="min-height: 400px;">
                         <div class="text-center">
-                            <h2>This page is under refining process</h2>
-                            <div style="font-size: 16px; margin-top: 20px">We'll be ready soon with a new tailored page.</div>
+                            <h2 class="no-background">This page is under refining process</h2>
+                            <div class="small-border"></div>
+                            <div style="font-size: 14px; margin-top: 20px">We'll be ready soon with a new tailored page.</div>
                         </div>
                     </div>
                 </div>
