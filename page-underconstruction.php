56a3cd33be6a96b4d5dc6f04e3cb285177d7405a
Admin <powerlogic1992@gmail.com>
2015-03-02 16:17:18 +0800

    Added Page Test.

---
+++ page-underconstruction.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Under Construction Page
+Author: Juni Brosas
+*/
+get_header();
+
+?>
+
+    <section class="main-section with-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="well well-products well-main">
+                        asdasd
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+<?php get_footer();
