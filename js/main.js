67462c500d8c461e4df599c5ac136dc3dc954c16
Admin <powerlogic1992@gmail.com>
2015-03-02 13:00:25 +0800

    Added Page Test.

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ $(function(){
     var wellMain = $('.well-main');
     var wellSide = $('.well-products-sidebar');
     console.log( wellMain.height() );
+    wellSide.css( 'margin-top', wellMain.height() );
 });
