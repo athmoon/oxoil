f397d922a1099817e99044efe7319be91f2f8456
Admin <powerlogic1992@gmail.com>
2015-03-02 22:31:06 +0800

    Added Page Test.

--- footer.php
+++ footer.php
@@ -10,13 +10,30 @@
 					</p>
 				</div>
 				<div class="col-lg-4 col-sm-6">
-					<?php /*the_widget( 'Subscription Widget' ); */?>
-					<div class="input-group sexy-form">
+					<?php $base_url = get_site_url(); ?>
+					<div id="email-subscribers-2">
+						<link rel="stylesheet" media="screen" type="text/css" href="<?=$base_url ?>/wp-content/plugins/email-subscribers/widget/es-widget.css">
+						<script language="javascript" type="text/javascript" src="<?=$base_url ?>/wp-content/plugins/email-subscribers/widget/es-widget.js"></script>
+						<div>
+							<div class="es_msg"><span id="es_msg"></span></div>
+							<div class="input-group input-group-lg">
+							<span>
+								<input class="es_textbox_class form-control" name="es_txt_email" id="es_txt_email" onkeypress="if(event.keyCode==13) es_submit_page('<?=$base_url ?>')" value="" type="email" placeholder="Enter your email address">
+							</span>
+							<span class="input-group-btn">
+								<button class="btn btn-primary btn-blue" name="es_txt_button" id="es_txt_button"  onclick="return es_submit_page('<?=$base_url ?>')" type="submit">SUBSCRIBE</button>
+							</span>
+							</div>
+							<input name="es_txt_name" id="es_txt_name" value="" type="hidden">
+							<input name="es_txt_group" id="es_txt_group" value="Subscription" type="hidden">
+						</div>
+					</div>
+					<!--<div class="input-group sexy-form">
 						<input type="text" class="form-control" placeholder="Enter Email Address">
                         <span class="input-group-btn">
                             <button class="btn btn-primary btn-blue" type="button">SUBSCRIBE</button>
                         </span>
-					</div><!-- /input-group -->
+					</div>--><!-- /input-group -->
 				</div>
 			</div>
 
