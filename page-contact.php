12d520adb0c4fd8cd12c3c294265ec62ca2b65e8
Admin <powerlogic1992@gmail.com>
2015-03-03 22:21:53 +0800

    Added Page Test.

--- page-contact.php
+++ page-contact.php
@@ -13,7 +13,9 @@ get_header();
                     <div class="well well-products well-main" style="min-height: 400px;">
                         <div class="row" style="margin-top:20px;">
                             <div class="col-md-6">
-                                <?php echo do_shortcode('[cscf-contact-form]') ?>
+                                <div class="sexy-form">
+                                    <?php echo do_shortcode('[cscf-contact-form]') ?>
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <iframe width="100%" height="400" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/view?zoom=9&center=25.2048%2C55.2708&key=AIzaSyDtnQ-CYgsu1CGmG8RSpPra8tcw09lsG3o"></iframe>
