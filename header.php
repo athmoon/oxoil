3949d6db45fe175ce263392d58644225e2172f8a
Admin <powerlogic1992@gmail.com>
2015-03-04 22:56:59 +0800

    Added Page Test.

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <html class="ie ie8" <?php language_attributes(); ?> >
 <![endif]-->
 <!--[if !(IE 7) | !(IE 8) ]><!-->
-<html <?php language_attributes(); ?> ng-app="" ng-cloak>
+<html <?php language_attributes(); ?> ng-app="oxoilApp" ng-cloak>
 
 <!--<![endif]-->
 <head>
--- page-home.php
+++ page-home.php
@@ -26,7 +26,7 @@
 				<div class="well well-main" style="min-height: 300px;">
 					<h2 class="title">Oil Products</h2>
 					<div class="row" ng-controller="FeatureProductsController">
-						<span ng-init='featureProducts = <?=$featuredProducts?>'></span>
+						<span ng-init='initFeature(0)'></span>
 						<div class="col-md-4 post-nav-container">
 							<ul class="list-unstyled post-nav">
 								<li ng-repeat="product in featureProducts" ng-class="currentProduct.id == product.id ? 'active' : '' ">
