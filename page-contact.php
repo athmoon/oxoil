a16c21b70698f4785bc1626b25fb199f7a88321b
Admin <powerlogic1992@gmail.com>
2015-03-04 13:50:10 +0800

    Added Page Test.

--- page-contact.php
+++ page-contact.php
@@ -10,42 +10,40 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="info">
-                        <div class="row">
-
-                            <div class="col-md-4">
-                                <div class="col-md-3 text-center"><img src="http://metagra-dmcc.ae/wp-content/themes/metagra/images/tracker-logo.png" alt="Street Address"></div>
-                                <div class="col-md-9">
-                                    <h4>Street Address</h4>
-                                    <p>Office 1402, Platinum Tower, Cluster I <br>
-                                        P.O.Box: 38192, Jumeirah Lakes Towers <br>
-                                        Dubai, UAE</p>
-                                </div>
-                            </div>
-
-                            <div class="col-md-4">
-                                <div class="col-md-3 text-center"><img src="http://metagra-dmcc.ae/wp-content/themes/metagra/images/phone-icon.png" alt="Phone"></div>
-                                <div class="col-md-9">
-                                    <h4>Phone</h4>
-                                    <p>Tel: +971 4 369 9787</p>
-                                </div>
-                            </div>
-
-                            <div class="col-md-4">
-                                <div class="col-md-3 text-center"><img src="http://metagra-dmcc.ae/wp-content/themes/metagra/images/email-icon.png" alt="Email"></div>
-                                <div class="col-md-9">
-                                    <h4>Email</h4>
-                                    <p>info@metagra-dmcc.ae</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
                     <div class="well well-products well-main" style="min-height: 400px;">
                         <div class="row" style="margin-top:20px;">
+                            <div class="col-md-12">
+                                <div class="info">
+                                    <div class="row">
+
+                                        <div class="col-md-4">
+                                            <div class="col-md-3 text-center"><img src="http://metagra-dmcc.ae/wp-content/themes/metagra/images/tracker-logo.png" alt="Street Address"></div>
+                                            <div class="col-md-9">
+                                                <h4>Street Address</h4>
+                                                <p>Office 1402, Platinum Tower, Cluster I <br>
+                                                    P.O.Box: 38192, Jumeirah Lakes Towers <br>
+                                                    Dubai, UAE</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4">
+                                            <div class="col-md-3 text-center"><img src="http://metagra-dmcc.ae/wp-content/themes/metagra/images/phone-icon.png" alt="Phone"></div>
+                                            <div class="col-md-9">
+                                                <h4>Phone</h4>
+                                                <p>Tel: +971 4 369 9787</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4">
+                                            <div class="col-md-3 text-center"><img src="http://metagra-dmcc.ae/wp-content/themes/metagra/images/email-icon.png" alt="Email"></div>
+                                            <div class="col-md-9">
+                                                <h4>Email</h4>
+                                                <p>info@metagra-dmcc.ae</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="col-md-6">
                                 <div class="sexy-form">
                                     <?php echo do_shortcode('[cscf-contact-form]') ?>
