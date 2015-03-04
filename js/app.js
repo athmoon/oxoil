8bd5712e76f48ae2ed2afca7af236c003580e423
Admin <powerlogic1992@gmail.com>
2015-03-04 22:49:59 +0800

    Added Page Test.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
-var app = angular.module('oxoilApp', []);
+//var app = angular.module('oxoilApp', []);
 
 
-app.controller('MainController', function( $scope ){
+//app.controller('MainController', function( $scope ){
+//    console.log('angularJS is working properly');
+//});
+
+function MainController($scope){
     console.log('angularJS is working properly');
-});
+}
 
+function FeatureProductsController($scope){
+    console.log($scope);
+}
+
+/*
 app.controller('FeatureProductsController', ['$scope', function( $scope ){
-    /*$scope.featureProducts = [];*/
+    */
+/*$scope.featureProducts = [];*//*
+
     console.log($scope.featureProducts);
-    /*$scope.init = function(productData){
+    */
+/*$scope.init = function(productData){
      $scope.featureProducts = productData;
-     }*/
-    /*$scope.featureProducts = [
+     }*//*
+
+    */
+/*$scope.featureProducts = [
      { id: 1, title : 'Base Oil', photo:  'http://www.argusmedia.com.br/~//~/media/Images/Hero-Box-530-by-335/Petroleum/Base-Oil.jpg', description: 'Base oils are used to manufacture products including lubricating greases, motor oil and metal processing fluids. Different products require different compositions and properties in the oil. One of the most important factors is the liquid’s viscosity at various temperatures.' },
      { id: 2, title : 'Fuel with Sulfur', photo:  'http://www.severnfuels.co.uk/images/gallery/road_forecourt_key_fuels.jpg', description: 'Fuels are any materials that store potential energy in forms that can be practicably released and used for work or as heat energy. ' },
      { id: 3, title : 'Gasoline', photo:  'http://www.thehindu.com/multimedia/dynamic/00497/IN08_GASOLINE1_497253f.jpg', description: 'Gasoline, also spelled gasolene, also called gas or petrol, mixture of volatile, flammable liquid hydrocarbons derived from petroleum and used as fuel for internal-combustion engines. It is also used as a solvent for oils and fats.' },
@@ -25,6 +39,7 @@ app.controller('FeatureProductsController', ['$scope', function( $scope ){
      $scope.featureSelect = function( id ){
      $scope.currentProduct = $scope.featureProducts[id];
      console.log(id);
-     }*/
+     }*//*
 
-}]);
+
+}]);*/
