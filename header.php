ab15630f36bfc46ebc5e018bd62b9b347a0f5324
Admin <powerlogic1992@gmail.com>
2015-03-04 22:51:40 +0800

    Added Page Test.

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?> ng-app="oxoilApp">
+<html class="ie ie7" <?php language_attributes(); ?> >
 <![endif]-->
 <!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?> ng-app="oxoilApp">
+<html class="ie ie8" <?php language_attributes(); ?> >
 <![endif]-->
 <!--[if !(IE 7) | !(IE 8) ]><!-->
-<html <?php language_attributes(); ?> ng-app="oxoilApp" ng-cloak>
+<html <?php language_attributes(); ?> ng-app="" ng-cloak>
 
 <!--<![endif]-->
 <head>
