911aa041db492dd2bebc45a33dc03460c9f18547
Admin <powerlogic1992@gmail.com>
2015-03-05 14:07:28 +0800

    Added Page Test.

--- page-about.php
+++ page-about.php
@@ -17,6 +17,16 @@
                 </div>
             </div>
         </div>
+        <div class="row" style="margin-top: 50px;">
+            <div class="col-md-6">
+                <img src="http://www.aecom.com/deployedfiles/Internet/Homepage/Kuwait_Oil_Company_690x355.jpg" alt="About Us" title="About Us" class="img-responsive" />
+            </div>
+            <div class="col-md-6">
+                <h3 class="header-with-border">Focal Point on Trading</h3>
+                <h2 class="header-normal">We provide energy behind phenomenal growth.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
     </div>
 </section>
 
