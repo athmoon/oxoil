70ff9a621e064c024e633f591ba1fc93cedb10b8
Admin <powerlogic1992@gmail.com>
2015-03-05 12:41:38 +0800

    Added Page Test.

--- js/app.js
+++ js/app.js
@@ -13,18 +13,9 @@ app.controller('FeatureProductsController', ['$scope', function( $scope ){
         $scope.featureProducts = productData;
         $scope.currentProduct = $scope.featureProducts[0];
     };
-/*
-$scope.featureProducts = [
-     { id: 1, title : 'Base Oil', photo:  'http://www.argusmedia.com.br/~//~/media/Images/Hero-Box-530-by-335/Petroleum/Base-Oil.jpg', description: 'Base oils are used to manufacture products including lubricating greases, motor oil and metal processing fluids. Different products require different compositions and properties in the oil. One of the most important factors is the liquid’s viscosity at various temperatures.' },
-     { id: 2, title : 'Fuel with Sulfur', photo:  'http://www.severnfuels.co.uk/images/gallery/road_forecourt_key_fuels.jpg', description: 'Fuels are any materials that store potential energy in forms that can be practicably released and used for work or as heat energy. ' },
-     { id: 3, title : 'Gasoline', photo:  'http://www.thehindu.com/multimedia/dynamic/00497/IN08_GASOLINE1_497253f.jpg', description: 'Gasoline, also spelled gasolene, also called gas or petrol, mixture of volatile, flammable liquid hydrocarbons derived from petroleum and used as fuel for internal-combustion engines. It is also used as a solvent for oils and fats.' },
-     { id: 4, title : 'Base Oil', photo:  'http://www.argusmedia.com.br/~//~/media/Images/Hero-Box-530-by-335/Petroleum/Base-Oil.jpg', description: 'Base oils are used to manufacture products including lubricating greases, motor oil and metal processing fluids. Different products require different compositions and properties in the oil. One of the most important factors is the liquid’s viscosity at various temperatures.' },
-     { id: 5, title : 'Fuel with Sulfur', photo:  'http://www.severnfuels.co.uk/images/gallery/road_forecourt_key_fuels.jpg', description: 'Fuels are any materials that store potential energy in forms that can be practicably released and used for work or as heat energy. ' },
-     { id: 6, title : 'Gasoline', photo:  'http://www.thehindu.com/multimedia/dynamic/00497/IN08_GASOLINE1_497253f.jpg', description: 'Gasoline, also spelled gasolene, also called gas or petrol, mixture of volatile, flammable liquid hydrocarbons derived from petroleum and used as fuel for internal-combustion engines. It is also used as a solvent for oils and fats.' }
-     ];*/
+
      $scope.featureSelect = function( product ){
          $scope.currentProduct = product;
-         //console.log(id);
      }
 
 
