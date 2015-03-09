ae8f545871e7a2a4b5afe58e06efcfa1e95fab7f
Admin <powerlogic1992@gmail.com>
2015-03-09 17:49:49 +0800

    updated about page.

--- page-about.php
+++ page-about.php
@@ -18,20 +18,12 @@
         <div  style="margin-top: 200px;">
             <div class="row" style="margin-bottom: 50px;">
                 <div class="col-md-6">
-                    <img src="<?=asset('images/who-we-are.jpg')?>" alt="About Us" title="About Us" class="img-responsive" />
+                    <img src="<?=asset('images/energy-oil_rig.jpg')?>" alt="About Us" title="About Us" class="img-responsive" />
                 </div>
                 <div class="col-md-6">
-                    <h3 class="header-with-border">Who We Are?</h3>
-                    <p>Oxoil Trading DMCC is an international trading company based in Dubai (UAE) and office in Ukraine. The company's main business activities include international trade, the import and export of bulk commodities. We have direct access to some of the world's biggest suppliers of bulk commodities, who have the capability to deliver products to qualified clients on time and as committed. We are dealing with various commodities in Food, Metals, Energy and Chemicalst. With our system of ongoing analyses of market prices, efficient information tools combined with in-depth knowledge and 20 years experience of market products and players, we ensure any buying or selling operation is performed rapidly and at the right price. Our activities are founded on the principle of providing our clients the commodities they request, in the shortest time possible, and the best competitive prices available in the market.We are continuously striving for supplying the best quality products to our valued customer around the globe. Our Major markets are Middle East and Europe.</p>
-                </div>
-            </div>
-            <div class="row" style="margin-bottom: 50px;">
-                <div class="col-md-6">
-                    <img src="<?=asset('images/what-we-do.jpg')?>" alt="About Us" title="About Us" class="img-responsive" />
-                </div>
-                <div class="col-md-6">
-                    <h3 class="header-with-border">What We Do?</h3>
-                    <p>The company's main business activities include international trade, the import and export of bulk commodities. We have direct access to some of the world's biggest suppliers of bulk commodities, who have the capability to deliver products to qualified clients on time and as committed.We are dealing with various commodities in Grain, Metals, Energy and Investment projects.</p>
+                    <h3 class="header-with-border">Focal Point on Trading</h3>
+                    <h1>High Standard Fuel Oil Standing</h1>
+                    <p>Oxoil Trading DMCC is an international oil trading company based in Dubai (UAE) and office in Ukraine. The company's main business activities include international trade, the import and export of bulk commodities. We have direct access to some of the world's biggest suppliers of bulk commodities, who have the capability to deliver products to qualified clients on time and as committed. We are dealing with various commodities in Food, Metals, Energy and Chemicalst. With our system of ongoing analyses of market prices, efficient information tools combined with in-depth knowledge and 20 years experience of market products and players, we ensure any buying or selling operation is performed rapidly and at the right price. Our activities are founded on the principle of providing our clients the commodities they request, in the shortest time possible, and the best competitive prices available in the market.We are continuously striving for supplying the best quality products to our valued customer around the globe. Our Major markets are Middle East and Europe.</p>
                 </div>
             </div>
         </div>
