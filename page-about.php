e0fe61cf220f5f0208dcbca44ab23e457a520b4d
Admin <powerlogic1992@gmail.com>
2015-03-09 17:53:51 +0800

    updated about page.

--- page-about.php
+++ page-about.php
@@ -22,7 +22,7 @@
                 </div>
                 <div class="col-md-6">
                     <h3 class="header-with-border">Focal Point on Trading</h3>
-                    <h1>High Standard Fuel Oil Standing</h1>
+                    <h1 class="header-normal">High Standard Fuel Oil Standing</h1>
                     <p>Oxoil Trading DMCC is an international oil trading company based in Dubai (UAE) and office in Ukraine. The company's main business activities include international trade, the import and export of bulk commodities. We have direct access to some of the world's biggest suppliers of bulk commodities, who have the capability to deliver products to qualified clients on time and as committed. We are dealing with various commodities in Food, Metals, Energy and Chemicalst. With our system of ongoing analyses of market prices, efficient information tools combined with in-depth knowledge and 20 years experience of market products and players, we ensure any buying or selling operation is performed rapidly and at the right price. Our activities are founded on the principle of providing our clients the commodities they request, in the shortest time possible, and the best competitive prices available in the market.We are continuously striving for supplying the best quality products to our valued customer around the globe. Our Major markets are Middle East and Europe.</p>
                 </div>
             </div>
