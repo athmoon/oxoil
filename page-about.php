cbb6e91e2d6a8c5930472c971b028cb8ef2c921a
Admin <powerlogic1992@gmail.com>
2015-03-09 15:57:12 +0800

    updated contact page.

--- page-about.php
+++ page-about.php
@@ -16,13 +16,29 @@
             </div>
         </div>
         <div class="row" style="margin-top: 200px; margin-bottom: 50px;">
-            <div class="col-md-6">
-                <img src="<?=asset('images/energy-oil_rig.jpg')?>" alt="About Us" title="About Us" class="img-responsive" />
+            <!--<div class="col-md-6">
+                <img src="<?/*=asset('images/energy-oil_rig.jpg')*/?>" alt="About Us" title="About Us" class="img-responsive" />
             </div>
             <div class="col-md-6">
                 <h3 class="header-with-border">Focal Point on Trading</h3>
                 <h2 class="header-normal">We provide energy behind phenomenal growth.</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>-->
+
+            <div class="col-md-6">
+                <img src="<?=asset('images/energy-oil_rig.jpg')?>" alt="About Us" title="About Us" class="img-responsive" />
+            </div>
+            <div class="col-md-6">
+                <h3 class="header-with-border">Who We Are?</h3>
+                <p>Metagra Trading DMCC is an international trading company based in Dubai (UAE) and office in Ukraine. The company's main business activities include international trade, the import and export of bulk commodities. We have direct access to some of the world's biggest suppliers of bulk commodities, who have the capability to deliver products to qualified clients on time and as committed. We are dealing with various commodities in Food, Metals, Energy and Chemicalst. With our system of ongoing analyses of market prices, efficient information tools combined with in-depth knowledge and 20 years experience of market products and players, we ensure any buying or selling operation is performed rapidly and at the right price. Our activities are founded on the principle of providing our clients the commodities they request, in the shortest time possible, and the best competitive prices available in the market.We are continuously striving for supplying the best quality products to our valued customer around the globe. Our Major markets are Middle East and Europe.</p>
+            </div>
+
+            <div class="col-md-6">
+                <img src="<?=asset('images/energy-oil_rig.jpg')?>" alt="About Us" title="About Us" class="img-responsive" />
+            </div>
+            <div class="col-md-6">
+                <h3 class="header-with-border">What We Do?</h3>
+                <p>The company's main business activities include international trade, the import and export of bulk commodities. We have direct access to some of the world's biggest suppliers of bulk commodities, who have the capability to deliver products to qualified clients on time and as committed.We are dealing with various commodities in Grain, Metals, Energy and Investment projects.</p>
             </div>
         </div>
     </div>
