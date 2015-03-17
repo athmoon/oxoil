4126b3028f2896fbb28e4fe20a3ce49d7a468e23
Admin <powerlogic1992@gmail.com>
2015-03-17 18:28:22 +0800

    updated about page.

--- page-news.php
+++ page-news.php
@@ -32,7 +32,7 @@ get_header();
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="short-feeds well-news">
+                <div class="short-feeds well-news well-products-sidebar">
                     <?php include('widgets/widget-ticker.php')?>
 
                     <div class="well">
