781b1727a842a0963007f4d22c236b1891dd9f85
Admin <powerlogic1992@gmail.com>
2015-03-17 17:37:16 +0800

    updated about page.

--- page-news.php
+++ page-news.php
@@ -32,16 +32,16 @@ get_header();
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="well-news well-products-sidebar">
-                    <div style="margin-bottom: 20px;">
-                        <?php include('widgets/widget-ticker.php')?>
-                    </div>
-                    <?php include('widgets/widget-short-feeds.php') ?>
-                    <!--<div class="well well-products-sidebar " style="padding: 15px;">
+                <div class="">
+                    <!--<div style="margin-bottom: 20px;">
+                        <?php /*include('widgets/widget-ticker.php')*/?>
+                    </div>-->
+                    <?php /*include('widgets/widget-short-feeds.php') */?>
+                    <div class="well well-products-sidebar well-news" style="padding: 15px;">
                         <h2 class="title" style="margin-bottom: 10px;">Commodity Index</h2>
 
-                        <?php /*include('widgets/widget-commodity.php') */?>
-                    </div>-->
+                        <?php include('widgets/widget-commodity.php') ?>
+                    </div>
                 </div>
 
             </div>
